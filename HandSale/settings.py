43f5cf1bb128d2d930565b382bacc70d6c25dfbf
Virgilio Padron Batun <virgilio.padron.sop.j@gmail.com>
2019-04-17 19:06:12 -0500

    Hello

--- HandSale/settings.py
+++ HandSale/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pointsale',
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
